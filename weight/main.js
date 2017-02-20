af40b554872717043b2706ce6dae9370969b506e
Christian <christian.koessel@gmx.de>
2017-02-20 16:25:40 +0100

    Aktuelles Gewicht speichern

--- weight/main.js
+++ weight/main.js
@@ -1,18 +1,28 @@
 'use strict';
 
-const express = require("express");
-const morgan = require("morgan");
-const serveStatic = require("serve-static");
+const express = require('express');
+const morgan = require('morgan');
+const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const PORT = 8080;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(serveStatic(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/', function (request, response) {
-    response.send('Hello World\n');
+app.post('/weight', function (request, response) {
+    var weightTable = {
+        'date': new Date(),
+        'weight': request.body.weight
+    }
+    fs.appendFile(__dirname + '/data/weight.json', JSON.stringify(weightTable));
+    response.redirect('/');
 });
-app.listen(PORT);
 
+app.listen(PORT);
 console.log('Server running at http://localhost:' + PORT);
